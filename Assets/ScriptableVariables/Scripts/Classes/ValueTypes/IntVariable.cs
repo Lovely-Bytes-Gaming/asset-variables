8a1b0c7bfd8da3fbcdc183e9c7b3807e22c733fc
BorisBodycheck <thegreatwormspirit@gmail.com>
2021-07-15 21:01:36 +0200

    moved templates for code generator into text files

--- Assets/ScriptableVariables/Scripts/Classes/ValueTypes/IntVariable.cs
+++ Assets/ScriptableVariables/Scripts/Classes/ValueTypes/IntVariable.cs
@@ -6,9 +6,6 @@ namespace CustomLibs.Util.ScriptableVariables
     [CreateAssetMenu(menuName = "Scriptable Objects/Value Types/Int")]
     public class IntVariable : ValueType<int> 
     { 
-        void Reset()
-        {
-            m_Value = 0;
-        }
+        void Reset() => m_Value = 0;
     };
 }
