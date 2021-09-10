4d74b02518b3de244539615f1a3fbec465c5eff5
Nicholas Graetz <nicholas.graetz@jointhespree.com>
2021-09-10 22:43:38 +0200

    prefab pool

---
+++ Assets/ScriptableVariables/Scripts/Classes/Utils/Poolable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Poolable : MonoBehaviour
+{
+    private PrefabPool parentPool;
+
+    public void Spawn(PrefabPool parentPool)
+    {
+        // already spawned by another pool
+        if (this.parentPool) return;
+        
+        this.parentPool = parentPool;
+        gameObject.SetActive(true);
+
+        ISpawnCallbackReceiver[] spawnCallbackReceivers =
+            GetComponentsInChildren<ISpawnCallbackReceiver>();
+
+        for (int i = 0; i < spawnCallbackReceivers.Length; ++i)
+            spawnCallbackReceivers[i].OnSpawn();
+    }
+    public void Despawn()
+    {
+        IDespawnCallbackReceiver[] spawnCallbackReceivers =
+            GetComponentsInChildren<IDespawnCallbackReceiver>();
+
+        for (int i = 0; i < spawnCallbackReceivers.Length; ++i)
+            spawnCallbackReceivers[i].OnDespawn();
+        
+        parentPool.RemoveInstance(gameObject);
+        parentPool = null;
+    }
+    
+    public interface ISpawnCallbackReceiver
+    {
+        void OnSpawn();
+    }
+
+    public interface IDespawnCallbackReceiver
+    {
+        void OnDespawn();
+    }
+}
---
+++ Assets/ScriptableVariables/Scripts/Classes/Utils/PrefabPool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Prefab Pool")]
+public class PrefabPool : ScriptableObject
+{
+    [SerializeField, Tooltip("Prefab type spawned by this pool")]
+    private Poolable template;
+
+    [SerializeField]
+    private int 
+        maxCapacity;
+
+    private Stack<GameObject> unusedObjects;
+
+    private void OnEnable()
+    {
+        unusedObjects = new Stack<GameObject>();
+    }
+
+    public GameObject SpawnInstance()
+    {
+        GameObject go;
+
+        if (unusedObjects.Count < 1)
+            go = Instantiate(template.gameObject);
+        else
+            go = unusedObjects.Pop();
+
+        go.GetComponent<Poolable>().Spawn(this);
+
+        return go;
+    }
+
+    public GameObject[] SpawnInstanceArray(int num)
+    {
+        GameObject[] outGOs = new GameObject[num];
+        for(int i = 0; i < num; ++i)
+        {
+            outGOs[i] = SpawnInstance();
+        }
+        return outGOs;
+    }
+
+    public GameObject SpawnInstance(Vector3 atPosition)
+    {
+        GameObject go;
+
+        if (unusedObjects.Count < 1)
+            go = Instantiate(template.gameObject);
+        else
+            go = unusedObjects.Pop();
+
+        go.transform.position = atPosition;
+        go.GetComponent<Poolable>().Spawn(this);
+
+        return go;
+    }
+
+    public void RemoveInstance(GameObject go)
+    {
+        go.transform.parent = null;
+        if(unusedObjects.Count >= maxCapacity)
+        {
+            Destroy(go);
+        }
+        else
+        {
+            go.SetActive(false);
+            unusedObjects.Push(go);
+
+        }
+    }
+}
